118d94e5336db3f2839c38240f9b26ef44d6bd5b
ViniciusGomes <viniciuslucasvlcg@gmail.com>
2025-04-27 20:06:17 -0300

    Começando a implementar as funcionalidades da página/chat

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ const userMessage = document.getElementsByClassName('user-message')
 const inputMessage = document.getElementById('user-input')
 const sendArquive = document.getElementById('send-arquive')
 const sendMessage = document.getElementById('send-message')
+const chat = document.getElementById('chat-bot')
+const chatPopup = document.getElementById('chat-popup')
+
+function closeOpenChat() {
+  const display = chat.style.display === 'none' ? 'block' : 'none'
+  chat.style.display = display
+  console.log(chat.style.display)
+}
+
+headerClose.addEventListener('click', closeOpenChat)
+chatPopup.addEventListener('click', closeOpenChat)
