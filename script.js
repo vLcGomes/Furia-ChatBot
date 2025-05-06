b5988ebf37bc70054eabb5e60749a20af130e42c
ViniciusGomes <viniciuslucasvlcg@gmail.com>
2025-05-06 11:15:54 -0300

    Alterações de Funcionalidades e download de modulos

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ const questionClass = 'user-message'
 const RespClass = 'bot-message'
 
 const headerClose = document.getElementById('chat-bot-header-close')
-const botMessage = document.getElementsByClassName('bot-message')
-const userMessage = document.getElementsByClassName('user-message')
 const inputMessage = document.getElementById('user-input')
 const sendArquive = document.getElementById('send-arquive')
 const sendMessage = document.getElementById('send-message')
@@ -12,22 +10,29 @@ const chat = document.getElementById('chat-bot')
 const chatPopup = document.getElementById('chat-popup')
 const chatBotField = document.getElementById('chat-bot-content')
 
+
 function closeOpenChat() {
   const display = chat.style.display === 'none' ? 'block' : 'none'
   chat.style.display = display
   console.log(chat.style.display)
 }
 
-function chatBotMessageSchedule() {
-  const message = document.createElement('div')
+function chatBotMessageSchedule(message) {
+  const messageBox = document.createElement('div')
   const imgBot = document.createElement('img')
+  const msg = document.createElement('div')
   imgBot.src = furiaLogoPath
-  message.id = 'bot-message-box'
-  
+  messageBox.id = 'bot-message-box'
+  msg.className = RespClass
+  messageBox.appendChild(imgBot)
+  messageBox.appendChild(message)
+  return messageBox
 }
 
 function chatBotResp(message) {
-
+  const botMessageBox = chatBotMessageSchedule(message)
+  botMessageBox.innerText(requestResponse(message))
+  chatBotField.appendChild(botMessageBox)
 }
 
 function displayUserMessage(message) {
@@ -36,14 +41,24 @@ function displayUserMessage(message) {
   upMessage.textContent = message
   upMessage.className = questionClass
   chatBotField.appendChild(upMessage)
+
 }
+
+
+//--------------------------------------------------------------
 
 
 headerClose.addEventListener('click', closeOpenChat)
 chatPopup.addEventListener('click', closeOpenChat)
 
-
 sendMessage.addEventListener('click', () => {
   displayUserMessage(inputMessage.value)
   inputMessage.value = ''
+})
+
+inputMessage.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') {
+    displayUserMessage(inputMessage.value)
+    inputMessage.value = ''
+  }
 })
